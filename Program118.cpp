c4821a6817cd63aa5f2726d47a8a9151b39613f1
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-01 09:48:07 -0700

    Program to display Jay Ganesh.

---
+++ Program118.cpp
@@ -0,0 +1,41 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : main()
+//  Input : --
+//  Output : Jay Ganesh
+//  Description : Program to display Jay Ganesh.
+//  Date : 1/08/2021
+//  Author : Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh from C++\n";  // printf("Jay Ganesh from C++\n");
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
