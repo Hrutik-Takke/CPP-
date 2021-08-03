a2389b14a984f123f99e42972adc45ab19a704cc
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-03 09:24:38 -0700

    Accept a file name from user and open that file and read data of that file.

---
+++ Program138.cpp
@@ -0,0 +1,58 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : FileX, ReadData
+//  Input : string
+//  Output :  
+//  Description : Accept a file name from user and open that file and read data of that file.
+//  Date : 03/08/2021
+//  Author : Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<io.h>
+#include<iostream>
+using namespace std;
+
+class FileX
+{
+public:
+    int fd;
+    
+    FileX(char Name[])
+    {
+        fd = open(Name,O_RDONLY);
+        if(fd == -1)
+        {   cout<<"Unable to open file\n";}
+         else
+         {
+            cout<<"File SuccessFully Opened..\n";
+         }
+    }
+    ~FileX()
+    {
+        close(fd);
+    }
+
+    void ReadData()
+    {
+        char Arr[20]={'\0'};
+        read(fd,Arr,10);
+        // printf("%s",Arr);
+        cout<<Arr;
+
+    }
+};
+int main()
+{
+    char str[30];
+    cout<<"Enter file name\n";
+    cin>>str;
+    FileX obj(str);
+    obj.ReadData();
+    return 0;
+}
