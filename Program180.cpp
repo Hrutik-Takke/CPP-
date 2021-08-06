7336e92a4ae5188166eb7d61359ddc4ea89a3de2
Hrutik-Takke <87705391+Hrutik-Takke@users.noreply.github.com>
2021-08-06 07:36:53 -0700

    Accept two numbers from user and swap two numbers using (Generic Function).

---
+++ Program180.cpp
@@ -0,0 +1,40 @@
+
+/////////////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name : Swap
+//  Input: ---- , ----
+//  Output: ----
+//  Description : Accept two numbers from user and swap two numbers using (Generic Function).
+//  Date: 06/08/2021
+//  Author: Hrutik Takke
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+template <class T>
+T Max(T iNo1, T iNo2)
+{
+    if(iNo1 > iNo2)
+    {
+        return iNo1;
+    }
+    else
+    {
+        return iNo2;
+    }
+}
+int main()
+{
+    int iValue1 , iValue2, iRet = 0 ;
+    
+    cout<<"Enter first value\n";
+    cin>>iValue1;
+    
+    cout<<"Enter second value\n";
+    cin>>iValue2;
+    iRet = Max(iValue1, iValue2);
+    cout<<"Maximum is : "<<iRet<<"\n";
+    return 0;
+}
